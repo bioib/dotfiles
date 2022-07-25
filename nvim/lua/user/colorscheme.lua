ccf500c55dcc7fb16f6e67e41c7406a9a7d8cfb4
Bioib <revandvoey@gmail.com>
2022-07-25 20:58:27 +0700

    add colorscheme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
 try
-  colorscheme nord
+  colorscheme onedark
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -59,6 +59,7 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use "shaunsingh/nord.nvim" 
+  use "navarasu/onedark.nvim"
   
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
