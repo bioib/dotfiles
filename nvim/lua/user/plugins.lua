60ede7e9419c987732ec1c3041e973444cc7a354
Bioib <revandvoey@gmail.com>
2022-07-29 01:14:53 +0700

    add lualine

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -73,6 +73,9 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip"
   use "saadparwaiz1/cmp_luasnip"
 
+  -- Statusline
+  use "nvim-lualine/lualine.nvim"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
