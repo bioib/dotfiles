7f84de2152d7a500265bacf35c09d3c85d6ebe0f
bioib <revandvoey@gmail.com>
2023-03-01 22:37:28 +0700

    feat(plugins): add more plugins

--- nvim/lua/bio/plugins.lua
+++ nvim/lua/bio/plugins.lua
@@ -1,8 +1,16 @@
--- Autocmd to sync packages on save
-vim.api.nvim_create_autocmd({"BufWritePost"}, {
-  pattern = "plugins.lua",
-  command = "so % | PackerSync",
-}) 
+local _ok, packer = pcall(require, "packer")
+
+if not _ok then
+  return
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
 
 require("packer").init({
   display = {
@@ -14,13 +22,25 @@ return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
 
   -- Colorscheme
-  -- use "shaunsingh/nord.nvim"
-  use 'marko-cerovac/material.nvim'
+  use 'navarasu/onedark.nvim'
 
   use 'sunjon/shade.nvim'
 
   -- lsp
   use "neovim/nvim-lspconfig"
   use "onsails/lspkind.nvim"
+
+  -- treesitter
+  use "nvim-treesitter/nvim-treesitter"
+
+  -- formatting
+  use { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end }
+  use { "numToStr/Comment.nvim", config = function() require("comment").setup() end }
+
+  -- discord rich presence
+  use "andweeb/presence.nvim"
+
+  use "nvim-tree/nvim-tree.lua"
+  use "nvim-tree/nvim-web-devicons"
 
 end)
